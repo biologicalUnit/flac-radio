71994b2bb424bec3f1d43d1deb2ab784fb2ab4c2
markryan1988 <markryan1988@69162062-f2ea-11dd-9fdb-3debd5008f28>
2009-02-10 19:57:56 +0000

    New Flac player - Improved interface for the GUI

---
+++ trunk/src/FlacPlayer.java
@@ -0,0 +1,54 @@
+import java.io.File;
+import javax.media.*;
+import apps.Decoder;
+
+public class FlacPlayer{
+	private File track;
+	private Player player;
+
+	public static void main(String []args){
+		FlacPlayer p = new FlacPlayer("track.wav");
+		p.load("/Users/markryan/Documents/TestDb/Rebel.flac");
+		double time = p.getTime();
+		int min = (int)time/60;
+		int sec = (int)time % 60;
+		System.out.println("Time: "+min+":"+sec);
+	}
+	//Constructor takes the name to save decoded track as in the form of a string ex:"track1.wav"
+	public FlacPlayer(String trackName){
+		player = null;
+		track = new File(trackName);
+	}
+	//Load decodes the track and creates a Realized Player
+	public void load(String filename){
+		Decoder p1 = new Decoder();
+		try{
+			p1.decode(filename,track.getPath());
+			player = Manager.createRealizedPlayer(track.toURL());
+		}catch(Exception e){e.printStackTrace();}
+	}
+	//Play starts the track from the beginning or from the last played part
+	public void play(){
+		try{
+			player.start();
+		}catch(Exception e){e.printStackTrace();}
+	}
+	//Stops the music from playing
+	public void pause(){
+		try{
+			player.stop();
+		}catch(Exception e){e.printStackTrace();}
+	}
+	
+	//Stops the track, resets it to the beginning
+	public void rewind(){
+		player.stop();
+		player.setMediaTime(player.RESET);
+	}
+	
+	//returns the length of the track in the form of seconds
+	public double getTime(){
+		return player.getDuration().getSeconds();
+	}
+
+}
